2f6ddb997bd292d98470478edf49dd3edd8cd0af
oeljeklaus-you <oeljeklaus2heart@gmail.com>
2018-07-12 17:39:27 +0800

    有序数组两数之后

---
+++ src/main/java/cn/edu/hust/array/_167_twoSum.java
@@ -0,0 +1,23 @@
+package cn.edu.hust.array;
+
+public class _167_twoSum {
+    //利用有序性特点即可
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result=new int[2];
+        int low=0;
+        int high=numbers.length-1;
+        while(low<high)
+        {
+            while(low<high&&numbers[low]+numbers[high]<target) low++;
+            while(low<high&&numbers[low]+numbers[high]>target) high--;
+            if(numbers[low]+numbers[high]==target)
+            {
+                result[0]=low+1;
+                result[1]=high+1;
+                break;
+            }
+
+        }
+        return result;
+    }
+}
