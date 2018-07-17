403ed54fd7337ad8a19c9ef25736e0fd4ef1525d
oeljeklaus-you <oeljeklaus2heart@gmail.com>
2018-07-17 15:39:31 +0800

    数组中第三大的数

--- src/main/java/cn/edu/hust/array/_414_thirdMax.java
+++ src/main/java/cn/edu/hust/array/_414_thirdMax.java
@@ -29,24 +29,25 @@ public class _414_thirdMax {
 
     //解法2，使用3个变量即可
     public int thirdMax2(int[] nums) {
-       Integer max1=null,max2=null,max3=null;
-       for(Integer num:nums)
-       {
-           if(max1==num||max2==num||max3==num) continue;
-           if(max1==null||num>max1)
-           {
-               max3=max2;
-               max2=max1;
-               max1=num;
-           }else if(max2==null||num>max2)
-           {
-               max2=max2;
-               max2=num;
-           }else if(max3==null||num>max3)
-           {
-               max3=num;
-           }
-       }
-       return max3==null?max1:max3;
+        Integer max1=null,max2=null,max3=null;
+        for(Integer num:nums)
+        {
+            //必须使用equals方法
+            if(num.equals(max1)||num.equals(max2)||num.equals(max3)) continue;
+            if(max1==null||num>max1)
+            {
+                max3=max2;
+                max2=max1;
+                max1=num;
+            }else if(max2==null||num>max2)
+            {
+                max3=max2;
+                max2=num;
+            }else if(max3==null||num>max3)
+            {
+                max3=num;
+            }
+        }
+        return max3==null?max1:max3;
     }
 }
