b33dbba31936b11f521b32678a64e8501afa118e
oeljeklaus-you <oeljeklaus2heart@gmail.com>
2018-07-17 19:06:35 +0800

    快乐数

---
+++ src/main/java/cn/edu/hust/array/_202_isHappy.java
@@ -0,0 +1,16 @@
+package cn.edu.hust.array;
+
+public class _202_isHappy {
+    //递归即可不过注意4会引起死循环
+    public boolean isHappy(int n) {
+        if(n==1) return true;
+        else if(n<=4&&n!=1) return false;
+        int result=0;
+        while(n!=0)
+        {
+            result+=(n%10)*(n%10);
+            n=n/10;
+        }
+        return isHappy(result);
+    }
+}
