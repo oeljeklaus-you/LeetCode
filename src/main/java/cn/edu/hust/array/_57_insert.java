08e0f5f568bd690cb1859775aff283ae39c76862
oeljeklaus-you <oeljeklaus2heart@gmail.com>
2018-08-14 17:12:35 +0800

    合并区间

--- src/main/java/cn/edu/hust/array/_57_insert.java
+++ src/main/java/cn/edu/hust/array/_57_insert.java
@@ -5,15 +5,15 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class _56_merge {
+public class _57_insert {
     class Interval {
-      int start;
-      int end;
-      Interval() { start = 0; end = 0; }
-      Interval(int s, int e) { start = s; end = e; }
+        int start;
+        int end;
+        Interval() { start = 0; end = 0; }
+        Interval(int s, int e) { start = s; end = e; }
     }
-    //按照自定义排序即可
-    public List<Interval> merge(List<Interval> intervals) {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        intervals.add(newInterval);
         Collections.sort(intervals, new Comparator<Interval>() {
             @Override
             public int compare(Interval o1, Interval o2) {
@@ -25,7 +25,6 @@ public class _56_merge {
             }
         });
         List<Interval> result=new ArrayList<Interval>();
-        if(intervals.size()==0) return result;
         Interval t=intervals.get(0);
         for(int i=1;i<intervals.size();i++)
         {
@@ -41,17 +40,5 @@ public class _56_merge {
         }
         result.add(t);
         return result;
-    }
-
-    public static void main(String[] args)
-    {
-        _56_merge merge=new _56_merge();
-        List<Interval> list=new ArrayList<Interval>();
-        list.add(merge.new Interval(2,3));
-        list.add(merge.new Interval(4,5));
-        list.add(merge.new Interval(6,7));
-        list.add(merge.new Interval(8,9));
-        list.add(merge.new Interval(1,10));
-        merge.merge(list);
     }
 }
