299886d491a7b17afaceb9d55af937c6011f4d1d
oeljeklaus-you <oeljeklaus2heart@gmail.com>
2018-09-19 14:54:02 +0800

    买卖股票的最佳时机 II

---
+++ src/main/java/cn/edu/hust/array/_122_maxProfit.java
@@ -0,0 +1,13 @@
+package cn.edu.hust.array;
+
+public class _122_maxProfit {
+    public int maxProfit(int[] prices) {
+        int total=0;
+        for(int i=0;i<prices.length-1;i++)
+        {
+            if(prices[i+1]>prices[i])
+                total+=prices[i+1]-prices[i];
+        }
+        return total;
+    }
+}
