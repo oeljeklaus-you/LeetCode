18077ad4fa725781bffdddc8b015a357e1370a13
oeljeklaus-you <oeljeklaus2heart@gmail.com>
2018-09-24 15:49:19 +0800

    搜索旋转排序数组 II

---
+++ src/main/java/cn/edu/hust/array/_81_serach.java
@@ -0,0 +1,31 @@
+package cn.edu.hust.array;
+
+public class _81_serach {
+    //这里需要考虑出现重复情况，如果含有重复元素，有可能那就一个一个比较
+    public boolean search(int[] nums, int target) {
+        if(nums==null) return false;
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(nums[mid]==target) return true;
+            else if(nums[start]==nums[mid]) start++;
+            else if(nums[start]<nums[mid])
+            {
+                if(target>=nums[start]&&target<nums[mid])
+                    end=mid-1;
+                else
+                    start=mid+1;
+            }
+            else
+            {
+                if(target>nums[mid]&&target<=nums[end])
+                    start=mid+1;
+                else
+                    end=mid-1;
+            }
+        }
+        return false;
+    }
+}
